0548e0549cc7b406635ffb7d2ac9c58718e5580c
litekmya <tekmya@mail.ru>
2024-06-03 16:39:57 +0300

    Initial Commit

---
+++ Effective Mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Effective Mobile
+//
+//  Created by Владимир Ли on 03.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
